feecb30210810180a413ba4b50fd990875bce3ac
aure-nogueras <anogueras@correo.ugr.es>
2020-11-19 15:13:51 +0100

    Updates

---
+++ precache-manifest.ca9c7f3d1a49521e1e49c4e834fa1209.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b4ab05c683d287aafece77867fe0551",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f161746860ce0e792b48",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "c1ca085f283bd7146e94",
+    "url": "/static/js/2.9dacb0b0.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.9dacb0b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f161746860ce0e792b48",
+    "url": "/static/js/main.d0073d6d.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "3f94ffc1051656d2292f4dcda2cd74ee",
+    "url": "/static/media/perfil.3f94ffc1.jpg"
+  },
+  {
+    "revision": "0ca0547ecbcbaac66bdf63cc2fe75187",
+    "url": "/static/media/resume.0ca0547e.pdf"
+  }
+]);
